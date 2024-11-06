eb23c662d1dab7f187981aebf84846837450fdf9
JayChauhan189 <jay189.chauhan@gmail.com>
2024-11-06 23:28:37 +0530

    [feat]: Implement handling for empty input

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,5 +1,12 @@
 export class StringCalculator {
     add(numbers: string): number {
+        let result = 0;
 
+        // Check if numbers is empty
+        if (numbers === "") {
+            return 0;
+        }
+
+        return result;
     }
 }
