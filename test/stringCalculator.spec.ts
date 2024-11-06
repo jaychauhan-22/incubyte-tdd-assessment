b0c069f7c302f94412c837b54d29136ab6ab02e0
JayChauhan189 <jay189.chauhan@gmail.com>
2024-11-06 23:32:28 +0530

    [test]: Add test for single number input

--- test/stringCalculator.spec.ts
+++ test/stringCalculator.spec.ts
@@ -1,15 +1,25 @@
 import { StringCalculator } from "../src/stringCalculator";
 
 describe("StringCalculatorAdd", () => {
+    // Given
+    let calc: StringCalculator;
+    
+    beforeEach(() => {
+        calc = new StringCalculator();
+    });
 
     test("shouldReturnZeroWhenInputIsEmpty", () => {
-        // Given
-        let calc = new StringCalculator();
-
         // When
         const result = calc.add("");
-
         // Then
         expect(result).toBe(0);
     });
+
+    test('shouldReturnNumberWhenInputHasSingleNumber', () => {
+        // When
+        const result = calc.add("1");
+        // Then
+        expect(result).toBe(1);
+    });
+    
 });
