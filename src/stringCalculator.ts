beda633b9810dd1db2605bd8408e031cdd473cfb
JayChauhan189 <jay189.chauhan@gmail.com>
2024-11-06 23:43:24 +0530

    [feat]: Implement handling for multiple number input

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,10 +1,21 @@
 export class StringCalculator {
     add(numbers: string): number {
         let result = 0;
-
+        
         // Check if numbers is empty
         if (numbers === "") {
             return 0;
+        }
+
+        // Split the numbers by delimeter ","
+        const nums = numbers.split(",");
+
+        // adding non-negative numbers and handling negative numbers
+        for (const num of nums) {
+            if (num !== "") {
+                const n = parseInt(num, 10);
+                result += n;
+            }
         }
 
         return result;
